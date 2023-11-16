c717c37a240abd8409ef054c837cdd4df38d0f26
JoanP26 <joanpenarrochacentelles6@gmail.com>
2023-11-16 22:47:08 +0100

    AcceptanceTests<HU05>: Invalid coordinates

--- src/modules/place-of-interest/place-of-interest.controller.spec.ts
+++ src/modules/place-of-interest/place-of-interest.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PlaceOfInterestController } from './place-of-interest.controller';
 import { PlaceOfInterestService } from './place-of-interest.service';
 import { PlaceOfInterest } from '../../entities/placeOfInterest.entity';
+import { HttpStatus } from '@nestjs/common';
 
 describe('PlacesOfInterestController (Alta Lugar de Interés - Válido)', () => {
   let placesController: PlaceOfInterestController;
@@ -35,6 +36,22 @@ describe('PlacesOfInterestController (Alta Lugar de Interés - Válido)', () => 
       coordinates: coordinates,
       favorite: false,
     });
+  });
+
+  it('debería lanzar una excepción si se intenta añadir un lugar de interés con coordenadas incorrectas', async () => {
+    // Limpiar la base de datos antes de la prueba
+    await placesService.clearDatabase();
+
+    // Intentar añadir un lugar de interés con coordenadas incorrectas
+    try {
+      await placesController.addPlaceOfInterestCoords('');
+      // Si no lanza una excepción, la prueba falla
+      fail('Se esperaba que lanzara una excepción');
+    } catch (error) {
+      // Verificar que la excepción sea la esperada
+      expect(error.status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+      expect(error.message).toBe('InvalidCoordinatesException');
+    }
   });
 
   // Limpiar la base de datos después de cada prueba si es necesario
