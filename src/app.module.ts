5c7566b2855feb0335a5b2c21890fcd9d5e17804
JoanP26 <joanpenarrochacentelles6@gmail.com>
2023-11-16 19:56:52 +0100

    Iteración-1 installed dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './modules/users/users.module';
-import { UsersService } from './modules/users/users.service';
-import { UsersController } from './modules/users/users.controller';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [UsersController],
-  providers: [UsersService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
