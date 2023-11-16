1c76f043f01dceda47a8736c72bb3144e1ed5ad2
JoanP26 <joanpenarrochacentelles6@gmail.com>
2023-11-16 19:51:36 +0100

    AcceptanceTests<HU02>: Invalid password

--- src/modules/users/user.controller.spec.ts
+++ src/modules/users/user.controller.spec.ts
@@ -37,49 +37,8 @@ describe('UsersController', () => {
     authController = module.get<AuthController>(AuthController);
     usService = module.get<UserService>(UserService);
     authService = module.get<AuthService>(AuthService);
-    jwtService = module.get<JwtService>(JwtService);
   });
 
-  it('Registra un usuario válido', async () => {
-    const user: RegisterDto = {
-      email: 'al386161@uji.es',
-      username: 'José Antonio',
-      password: 'Tp386161',
-    };
-
-    // Limpiar la base de datos antes de la prueba
-    await usService.clearDatabase();
-
-    // Realizar la solicitud al endpoint de registro
-    const response = await authController.register(user);
-
-    // Verificar la respuesta del controlador
-    expect(response.email).toEqual(user.email);
-
-    // Verificar que el usuario se haya registrado en el servicio
-    const usuariosRegistrados = await usService.getUsers();
-    expect(usuariosRegistrados).toHaveLength(1);
-  });
-
-  it('debería lanzar Database not accesible si la base de datos no está disponible', async () => {
-    // Simular que la base de datos no está disponible
-    jest.spyOn(authService, 'register').mockImplementation(async () => {
-      throw new HttpException('Database not accesible', HttpStatus.INTERNAL_SERVER_ERROR);
-    });
-  
-    // Realizar la solicitud al endpoint de registro
-    try {
-      await authController.register({
-        email: 'al386161@uji.es',
-        username: 'José Antonio',
-        password: 'Tp386161',
-      });
-    } catch (error) {
-      // Verificar que la respuesta sea un error 500 y contenga el mensaje esperado
-      expect(error.status).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
-      expect(error.message).toBe('Database not accesible');
-    }
-  });
   it('debería iniciar sesión con un usuario correcto y cargar datos de la base de datos', async () => {
     // Limpiar la base de datos antes de la prueba
     await usService.clearDatabase();
@@ -113,6 +72,69 @@ describe('UsersController', () => {
     }
     
   });
+
+  it('debería lanzar InvalidPasswordException para un usuario con contraseña incorrecta', async () => {
+    // Limpiar la base de datos antes de la prueba
+    await usService.clearDatabase();
+
+    // Crear un usuario en la base de datos
+    const user: User = {
+      id: 1,
+      username: 'José Antonio',
+      email: 'al386161@uji.es',
+      password: await bcrypt.hash('Tp386161', 10), // Hashear la contraseña antes de almacenarla
+    };
+    await authService.register(user);
+
+    // Realizar la solicitud al endpoint de inicio de sesión
+    const loginResponse = await userController.login({
+      email: 'al386161@uji.es',
+      password: 'Tp386161',
+    });
+    const accessToken = await jwtService.signAsync({ id: user.id, email: user.email });
+    // Verificar que la respuesta del controlador contiene el token
+    expect(loginResponse).toHaveProperty(accessToken);
+
+    // Decodificar el token para obtener la información del usuario
+    try {
+      const decoded = jwtService.decode(loginResponse.token);
+      // Verificar que el usuario en el token coincide con el usuario registrado
+      expect(decoded.email).toBe(user.email);
+    } catch (error) {
+      // Manejar el error (token inválido, expirado, etc.)
+      throw new Error('Error al decodificar el token');
+    }
+    
+  });
+
+  it('debería lanzar InvalidPasswordException para un usuario con contraseña incorrecta', async () => {
+    // Limpiar la base de datos antes de la prueba
+    await userService.clearDatabase();
+
+    // Crear un usuario en la base de datos
+    const user: User = {
+      id: 1,
+      username: 'José Antonio',
+      email: 'al386161@uji.es',
+      password: await bcrypt.hash('Tp386161', 10), // Hashear la contraseña antes de almacenarla
+    };
+    await userService.registerUser(user);
+
+    // Realizar la solicitud al endpoint de inicio de sesión con una contraseña incorrecta
+    try {
+      await userController.login({
+        email: 'al386161@uji.es',
+        password: 'tp386161', // Contraseña incorrecta
+      });
+    } catch (error) {
+      // Verificar que la respuesta sea un error 500 y contenga el mensaje esperado
+      expect(error.status).toBe(HttpStatus.UNAUTHORIZED);
+      expect(error.message).toBe('InvalidPasswordException');
+    }
+
+    // Verificar que se lanzó la excepción InvalidPasswordException
+  });
+
 
 // Limpiar la base de datos después de cada prueba si es necesario
   afterEach(async () => {
