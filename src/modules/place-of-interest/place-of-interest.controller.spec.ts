d63d72862a855ffeb6dab23806e2576b5f3701d9
Lucas <al386171@uji.es>
2023-11-16 20:09:09 +0100

    AcceptanceTests<HU06>: valid/invalid registered place of interest complete

--- src/modules/place-of-interest/place-of-interest.controller.spec.ts
+++ src/modules/place-of-interest/place-of-interest.controller.spec.ts
@@ -1,44 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlaceOfInterestController } from './place-of-interest.controller';
 import { PlaceOfInterestService } from './place-of-interest.service';
-import { HttpModule } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
+import { HttpException } from '@nestjs/common/exceptions';
+import { HttpStatus } from '@nestjs/common/enums';
 
-describe('PlaceOfInterestController E01 (Válido)', () => {
+describe('PuntosInteresController', () => {
   let controller: PlaceOfInterestController;
-  let poiService: PlaceOfInterestService;
+  let piService: PlaceOfInterestService;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule, DatabaseModule],
       controllers: [PlaceOfInterestController],
       providers: [PlaceOfInterestService],
     }).compile();
 
     controller = module.get<PlaceOfInterestController>(PlaceOfInterestController);
-    poiService = module.get<PlaceOfInterestService>(PlaceOfInterestService);
+    piService = module.get<PlaceOfInterestService>(PlaceOfInterestService);
   });
 
-  it('Crea un lugar de interés cuando el topónimo es "Castellón"', async () => {
-    // Given
+  it('Crea un nuevo punto de interés con un topónimo válido', async () => {
+    const topónimo = 'Castellón';
+    const coordenadas = '-0.0576800, 39.9929000';
+
+    // Simular que la API de geocoding está disponible y devuelve las coordenadas
+    jest.spyOn(piService, 'getCoordinatesFromAPI').mockResolvedValue(coordenadas);
+
+    // Simular que la base de datos está disponible
+    jest.spyOn(piService, 'savePointOfInterest').mockImplementation(async () => {
+      return {
+        id: 0,
+        name: topónimo,
+        coord: coordenadas,
+        fav: false,
+      };
+    });
+
+    // Realizar la solicitud para añadir un nuevo punto de interés
+    await controller.addPlaceOfInterest(topónimo);
+
+    // Verificar que se haya llamado a la función para obtener coordenadas con el topónimo correcto
+    expect(piService.getCoordinatesFromAPI).toHaveBeenCalledWith(topónimo);
+
+    // Verificar que se haya llamado a la función para guardar el punto de interés con los datos correctos
+    expect(piService.savePointOfInterest).toHaveBeenCalledWith({
+      name: topónimo,
+      coord: coordenadas,
+      fav: false,
+    });
+
+    // Verificar que el punto de interés se ha guardado en el servidor
+    const puntosInteres = await piService.getAllPointsOfInterest();
+    expect(puntosInteres).toHaveLength(1);
+    expect(puntosInteres[0]).toEqual({
+      name: topónimo,
+      coord: coordenadas,
+      fav: false,
+    });
+  });
+
+  it('debería lanzar un error si la API de geocoding no está disponible', async () => {
     const topónimo = 'Castellón';
 
-    // When
-    const result = await controller.addPlaceOfInterest(topónimo);
+    // Simular que la API de geocoding no está disponible
+    jest.spyOn(piService, 'getCoordinatesFromAPI').mockRejectedValue(new Error('GeocodingNotWorkingException'));
 
-    // Then
-    expect(result).toBeDefined();
-    expect(result.name).toBe('Castellon');
-    expect(result.coord).toBe('-0.0576800, 39.9929000');
-    expect(result.fav).toBe('false');
-
-    const puntosInteres = await poiService.getAllPlacesOfInterest();
-    expect(puntosInteres).toHaveLength(1);
-    expect(puntosInteres[0]).toEqual(result);
+    // Realizar la solicitud para añadir un nuevo punto de interés
+    try {
+      await controller.addPlaceOfInterest(topónimo);
+    } catch (error) {
+      // Verificar que se haya lanzado un error con el mensaje esperado
+      expect(error instanceof HttpException).toBe(true);
+      expect(error.getStatus()).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
+      expect(error.getResponse()).toBe('GeocodingNotWorkingException');
+    }
   });
 
-  afterAll(async () => {
-    // Limpiar la base de datos después de las pruebas si es necesario
-    await poiService.clearDatabase();
+  afterEach(async () => {
+    // Limpiar la base de datos después de cada prueba
+    await piService.clearPointsOfInterest();
   });
 });
--- src/modules/place-of-interest/place-of-interest.module.ts
+++ src/modules/place-of-interest/place-of-interest.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { PlaceOfInterestController } from './place-of-interest.controller';
-import { PlaceOfInterestService } from './place-of-interest.service';
-//import { DatabaseModule } from '../database/database.module';
 
 @Module({})
 export class PlaceOfInterestModule {}
