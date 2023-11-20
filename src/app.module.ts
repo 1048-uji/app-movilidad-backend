4e316d38ecdd90138addb2a03e2e53116b6dbd2e
JoanP26 <joanpenarrochacentelles6@gmail.com>
2023-11-20 11:41:14 +0100

    Database created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host:'roundhouse.proxy.rlwy.net',
+      port: 26383,
+      username: 'root',
+      password: 'EFA4E3ehDbc-3BAb5DFFHHbf6feH5HH5',
+      database: 'railway',
+      entities: [__dirname + '/**/*.entity[.ts,.js]'],
+      synchronize: true,
+    })
+  ],
   controllers: [],
   providers: [],
 })
