1e3a74119b94f430459d43397f36aecba512c2ff
JoanP26 <joanpenarrochacentelles6@gmail.com>
2023-12-04 17:04:27 +0100

    Bug fixed

--- src/modules/auth/strategy/jwt-auth.guard.ts
+++ src/modules/auth/strategy/jwt-auth.guard.ts
@@ -1,9 +1,10 @@
 import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard(['strategy_jwt_1', 'strategy_jwt_2', 'admin']) {
-    canActivate(context: ExecutionContext) {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean>  {
         // Add your custom authentication logic here
         // for example, call super.logIn(request) to establish a session.
         return super.canActivate(context);
