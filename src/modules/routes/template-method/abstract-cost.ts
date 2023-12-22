99c3b9f646e445383aeacb6aace54757ae1c6a3c
al386171 <al386171@uji.es>
2023-12-22 12:13:59 +0100

    Implementation<HU15>: Complete

--- src/modules/routes/template-method/abstract-cost.ts
+++ src/modules/routes/template-method/abstract-cost.ts
@@ -1,44 +1,38 @@
 import { Route } from "entities/route.entity";
 import { Vehicle } from "entities/vehicle.entity";
-import { RoutesController } from "../routes.controller";
 import { RoutesService } from "../routes.service";
 import { Repository } from 'typeorm';
-import { VehicleController } from "modules/vehicle/vehicle.controller";
 import { VehicleService } from "modules/vehicle/vehicle.service";
 
 export abstract class AbstractCost {
     private readonly routesRepository: Repository<Route>;
     private readonly vehicleRepository: Repository<Vehicle>;
-    private routeController: RoutesController;
     private routeService: RoutesService;
-    private vehicleController: VehicleController;
     private vehicleService: VehicleService;
 
     constructor() {
         this.routeService = new RoutesService(this.routesRepository);
-        this.routeController = new RoutesController(this.routeService); //¿Creamos un controller nuevo o se lo pasamos como argumento?
         this.vehicleService = new VehicleService(this.vehicleRepository);
-        this.vehicleController = new VehicleController(this.vehicleService);
     }
 
-    calcularCoste(vehicle: Vehicle, route: Route): Promise<number> {
-        const distance = this.getDistance(route);
-        const list = this.getConsum(vehicle);
+    async calcularCoste(vehicle: Vehicle, route: Route): Promise<number> {
+        const distance = await this.getDistance(route);
+        const list = await this.getConsum(vehicle);
         const type = list[0];
-        const consum = list[1];
-        const precio = this.getPrice(type, consum, distance);
+        const consum = parseFloat(list[1]);
+        const precio = await this.getPrice(type, consum, distance);
         return precio;
     }
 
-    getDistance(route: Route): Promise<number> {
-        return this.routeController.getDistance(route);
+    async getDistance(route: Route): Promise<number> {
+        return await this.routeService.getDistance(route);
     }
 
-    getConsum(vehicle: Vehicle): Promise<string> {
+    async getConsum(vehicle: Vehicle): Promise<string> {
         if (vehicle == null) {
             return Promise.resolve("walk");
         } else {
-            return this.vehicleController.getVehicleType(vehicle);
+            return await this.vehicleService.getVehicleType(vehicle);
         }
     }
 
--- src/modules/routes/template-method/electric-cost.ts
+++ src/modules/routes/template-method/electric-cost.ts
@@ -1,12 +1,20 @@
 import { Vehicle } from "entities/vehicle.entity";
 import { AbstractCost } from "./abstract-cost";
 import { HttpException, HttpStatus } from "@nestjs/common";
+import axios from "axios";
 class ElectricCost extends AbstractCost {
-    getPrice(type: string, consum: number, distance: number): Promise<number> {
+    async getPrice(type: string, consum: number, distance: number): Promise<number> {
         if (type != "electric") {
             throw new HttpException('InvalidTypeVehicleException', HttpStatus.UNAUTHORIZED);
         } else {
-            return //(distance / 100) * consum * llamar a la api de gasto electrico;
+            try {
+                const baseUrlLuz = 'https://api.preciodelaluz.org/v1/prices/avg?zone=PCB';
+                const response = await axios.get(baseUrlLuz);
+                const price = response.data.price;
+                return (distance / 100) * consum * price;
+            } catch (error) {
+                throw new HttpException('APINotWorkingException', HttpStatus.BAD_GATEWAY);
+            }
         }
     }
 }
