3ed4565f0cedbca0a14bfd5f3993de2c6163ad47
JoanP26 <joanpenarrochacentelles6@gmail.com>
2023-12-28 14:49:35 +0100

    Import VehicleRepository in UserService

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -4,9 +4,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from '../../entities/user.entity';
 import { JwtStrategy } from '../auth/strategy/jwt.strategy';
+import { Vehicle } from '../../entities/vehicle.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [TypeOrmModule.forFeature([User, Vehicle])],
     controllers: [UserController],
     providers: [UserService, JwtStrategy]
 })
